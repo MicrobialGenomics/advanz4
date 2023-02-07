7434033a00b86c1b2ff881b22a61387f0b6b1f49
cblazquez <cblazquez@irsicaixa.es>
2023-02-07 16:49:02 +0100

    Added poster figures to Taxonomy DA script

--- code/group_comparisons.R
+++ code/group_comparisons.R
@@ -1,3 +1,90 @@
+PairedWilcox_withNAs<-function(myDF, Variable, Response, Subsets=NULL, IDs){
+
+  # This function gets the matched paired samples in a longitudinal study and removes those groups that don't have all the members.
+  require(utils)
+
+  myInnerDF<-myDF %>%
+    dplyr::rename(Response = !!sym(Response),
+                  Variable = !!sym(Variable),
+                  IDs = !!sym(IDs))
+
+  subs <- myDF %>%
+    pull(Response) %>%
+    unique()
+
+  myPossiblePairs <-
+    combn(subs,2, simplify = F) %>%
+
+    map(function(comb){
+      nlev <-
+        myInnerDF %>%
+        dplyr::select(IDs, Variable, Response) %>%
+        dplyr::filter(Response %in% comb &
+                        !is.na(Variable)) %>%
+        pull(Response) %>%
+        unique() %>%
+        length()
+
+      if(nlev < 2) {
+        return(NULL)
+      } else {
+        return(comb)
+      }
+    }) %>%
+    purrr::discard(is.null)
+
+
+
+  myPossiblePairs %>%
+    purrr::set_names() %>%
+    purrr::map_dfr(function(set){
+
+      if(is.null(Subsets)){
+        try(
+          myTestDF <-
+            myInnerDF %>%
+
+            dplyr::filter(Response %in% set) %>%
+            dplyr::mutate(Variable = as.numeric(Variable),
+                          Response = as.character(Response)) %>%
+            tibble::as_tibble() %>%
+            dplyr::arrange(IDs, Response) %>%
+            dplyr::group_by(IDs) %>%
+            dplyr::filter(n()==2) %>%
+            dplyr::ungroup() %>%
+            rstatix::wilcox_test(.,Variable ~ Response, paired = T, p.adjust.method = "BH") %>%
+            rstatix::add_y_position() %>%
+            dplyr::select(-groups)
+        )
+
+      } else {
+        try(
+          myTestDF <-
+            myInnerDF %>%
+
+            dplyr::filter(Response %in% set,
+                          !is.na(Variable)) %>%
+            dplyr::mutate(Variable = as.numeric(Variable),
+                          Response = as.character(Response)) %>%
+            tibble::as_tibble() %>%
+            dplyr::arrange(IDs, Response) %>%
+            dplyr::group_by(IDs,!!sym(Subsets)) %>%
+            dplyr::filter(n()==2) %>%
+            dplyr::ungroup(IDs) %>%
+            rstatix::wilcox_test(.,Variable ~ Response, paired = T, p.adjust.method = "none")  %>%
+            rstatix::add_y_position() %>%
+            dplyr::select(-groups)
+        )
+
+      }
+
+
+    })
+
+}
+
+
+
 get_group_comparisons <- function(dat,
                                   link_var,
                                   long_var,
@@ -6,11 +93,13 @@ get_group_comparisons <- function(dat,
                                   num_vector,
                                   type = "categorical"){
 
+  # This is a function to obtain the stats of a certain conmparison, be it categorical or longitudinal. It returns pvals and statistical info (mean+IQR).
+
   stopifnot("argument 'type' must have one of the following values:
             'categorical' or 'longitudinal"=type %in% c("categorical", "longitudinal"))
 
 
-  if(type == "categorical"){
+  if(type == "categorical"){ # Doesn't need matching pairs, groups can have different Ns.
     cat_vector %>%
       purrr::set_names() %>%
       purrr::map(function(cv){
@@ -40,7 +129,7 @@ get_group_comparisons <- function(dat,
             return(list(stats = stats, test = wilc))
           })
       })
-  }else{
+  }else{ # Performs longitudinal test, keeps only those pairs of samples where both members are present
     cat_vector %>%
       purrr::set_names() %>%
       purrr::map(function(cv){
@@ -85,3 +174,111 @@ get_group_comparisons <- function(dat,
   }
 
 }
+
+
+get_comp_boxplots <-
+  function(dat,
+           cat_var,
+           num_var,
+           long_var,
+           link_var,
+           pal = "Accent",
+           y_trans = c("identity")) {
+
+    # set NULL transformation
+
+      trans_fun <- function(x){do.call(y_trans, list(x))}
+
+
+
+
+    df <-
+      dat  %>%
+      dplyr::select(SampleID,
+                    link_var = !!sym(link_var),
+                    long_var = !!sym(long_var),
+                    cat_var = !!sym(cat_var),
+                    num_var = !!sym(num_var)) %>%
+      dplyr::filter(!is.na(cat_var))
+
+
+    test_cat <-
+      df %>%
+      dplyr::group_by(long_var) %>%
+      rstatix::wilcox_test(num_var ~ cat_var, p.adjust.method = "BH") %>%
+      rstatix::add_significance(p.col = "p", output.col = "p.adj.signif") %>%
+      rstatix::add_xy_position("long_var", ) %>%
+      mutate(y.trans = trans_fun(y.position))
+
+
+
+    test_long <-
+      df %>%
+      PairedWilcox_withNAs(
+        myDF = .,
+        IDs = "link_var",
+        Variable = "num_var",
+        Response = "long_var",
+        Subsets = "cat_var"
+      ) %>%
+      group_by(cat_var) %>%
+      rstatix::adjust_pvalue(p.col = "p",
+                             output.col = "p.adj",
+                             method = "BH") %>%
+      ungroup() %>%
+      add_significance(p.col = "p.adj") %>%
+      rstatix::add_x_position(x = c("long_var"), group = "cat_var") %>%
+      mutate(y.trans = trans_fun(y.position))
+
+
+    boxplot <-
+      df %>%
+      ggplot(., aes(x = as.factor(long_var), y = trans_fun(num_var))) +
+      geom_boxplot(aes(fill = cat_var), position = "dodge", outlier.size = .6) +
+
+      # coord_trans(y="sqrt")+
+      # scale_y_continuous(labels = scales::trans_) +
+      ggpubr::stat_pvalue_manual(
+        data = test_cat,
+        label = "p.adj.signif",
+        hide.ns = T,
+        size = 6,
+        step.increase = 0.1,
+        bracket.size = .75,
+        y.position = "y.trans"
+      ) +
+      ggpubr::stat_pvalue_manual(
+        data = test_long,
+        label = "p.adj.signif",
+        hide.ns = T,
+        step.increase = 0.1,
+        size = 6,
+        bracket.size = .75,
+        y.position = "y.trans"
+
+      ) +
+      theme_bw() +
+      scale_fill_brewer(palette = pal) +
+      labs(y = num_var, x = "weeks", fill = cat_var) +
+      theme(
+        axis.text = element_text(size = 12),
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        legend.key.size = unit(1.3, "cm"),
+        legend.title = element_text(size = 12),
+        legend.text = element_text(size = 12)
+      )
+
+    return(list(test = list(cat = test_cat, long = test_long), plot = boxplot))
+
+
+  }
+    # Performs both categorical and longitudinal tests and places the in a boxplot.
+
+
+
+
+
+
+
+
